c4604bf75882d2ac8c10a404c441015c893d96dd
Pablo-Wisemen <accessdevelopmentjs@gmail.com>
2023-07-27 17:15:58 +0500

    1.0.0

--- Learn/src/main/kotlin/main.kt
+++ Learn/src/main/kotlin/main.kt
@@ -1,5 +1,15 @@
 fun main() {
-    val name:String? = "Alex"
+    var count = 700
+    val food = if (count > 1000) {
+       count -=200
+        "pizza"
+    } else if (count > 400) {
+       count -=400
+       "coffee"
+    } else {
+       "nothing"
+    }
 
-    println(name?:5)
+   println("I can take $food")
+
 }
